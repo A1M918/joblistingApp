27058e2cc5d90c38bd205b65da1800cb841bcdcc
Aamir <9.one.eight@gmail.com>
2020-06-14 21:57:53 +0500

    Profile screen navigating

--- App.js
+++ App.js
@@ -13,12 +13,39 @@ import {createStackNavigator} from 'react-navigation-stack';
 import LoginScreen from './src/Screens/LoginScreen';
 import SignUpScreen from './src/Screens/SignupScreen';
 import HomeScreen from './src/Screens/HomeScreen';
-// import ProfileScreen from './src/Screens/ProfileScreen';
 import CreateJobScreen from './src/Screens/CreateJobScreen';
+import ProfileScreen from './src/Screens/ProfileScreen';
+import {TouchableOpacity} from 'react-native';
+import {Icon} from 'react-native-elements';
+import * as NavigationService from './src/NavigationService/NavigationService';
+
+const ProfileButton = () => (
+  <TouchableOpacity onPress={() => NavigationService.navigate('Profile')}>
+    <Icon name="person" size={30} style={{marginRight: 15}} />
+  </TouchableOpacity>
+);
+
+const staticNavOptions = () => {
+  return {
+    navigationOptions: ({navigation}) => ({
+      // title: `${navigation.state.params.title}`,
+      title: 'Home',
+      // headerTitleStyle: styles.headerTitle,
+      // headerStyle: styles.header,
+      // headerTransparent: false,
+      // headerLeft: headerLeft(Images.blackLeftArrow, () => pop()),
+      headerRight: ProfileButton(),
+      //headerStyle,
+    }),
+  };
+};
 
 const userStack = createStackNavigator({
-  Home: HomeScreen,
-//   Profile: ProfileScreen,
+  Home: {
+    screen: HomeScreen,
+    ...staticNavOptions(),
+  },
+  Profile: ProfileScreen,
   CreateJob: CreateJobScreen,
 });
 
